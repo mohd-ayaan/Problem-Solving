7ac573690340faed1f97e1d48d0f2ae6e680ff89
mohd-ayaan <2k22.csaiml.2212406@gmail.com>
2025-04-09 19:17:14 +0530

    Time: 12 ms (55.35%), Space: 31.2 MB (82.7%) - LeetHub

---
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/3621-minimum-operations-to-make-array-values-equal-to-k.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int minval=*min_element(nums.begin(),nums.end());
+        int n=1, lastele=nums[0];
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]!=lastele){
+                lastele=nums[i];
+                n+=1;
+            }
+        }
+        cout<<minval<<endl;
+        if(k==minval){return n-1;}
+        if(k<minval){return n;}
+        return -1;
+    }
+};
