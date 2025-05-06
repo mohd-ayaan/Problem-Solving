b5b0b77f465268a04ae5afc3d8ae921a148e0929
mohd-ayaan <2k22.csaiml.2212406@gmail.com>
2025-05-06 18:09:25 +0530

    Time: 0 ms (100%), Space: 20.8 MB (18.1%) - LeetHub

--- 2048-build-array-from-permutation/2048-build-array-from-permutation.cpp
+++ 2048-build-array-from-permutation/2048-build-array-from-permutation.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
     vector<int> buildArray(vector<int>& nums) {
-        unordered_map<int,int> mp;
-        for(int i=0;i<nums.size();i++){
-            if(mp.find(nums[i])!=mp.end()){
-                mp[i]=nums[i];
-                nums[i]=mp[nums[i]];
-            }
-            else{
-                mp[i]=nums[i];
-                nums[i]=nums[nums[i]];
-            }
+        vector<int> ans;
+        int n = nums.size();
+        for(int i=0; i<n; i++){
+            ans.push_back(nums[nums[i]]);
         }
-        return nums;
+        return ans;
     }
 };
