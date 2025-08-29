0deb68a30e3cb3683af8018bb5e1e85249841655
mohd-ayaan <2k22.csaiml.2212406@gmail.com>
2025-08-29 12:15:12 +0530

    Time: 0 ms (100%), Space: 11 MB (29.59%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    void generatePermutations(vector<int>& nums, int index, vector<vector<int>>& result) {
-    // Base case: If index reaches the end, add the current array to the result
-    if (index == nums.size()) {
-        result.push_back(nums);
-        return;
+    void DFS(vector<int>& nums, vector<vector<int>>& res, unordered_set<int>& visited, vector<int>& subset){
+        if(subset.size()==nums.size()){
+            res.push_back(subset);
+        }
+        for(int i=0;i<nums.size();i++){
+            if(visited.find(i)==visited.end()){
+                visited.insert(i);
+                subset.push_back(nums[i]);
+                DFS(nums,res,visited,subset);
+                subset.pop_back();
+                visited.erase(i);
+            }
+        }
     }
-
-    // Recursive case: Swap and generate permutations
-    for (int i = index; i < nums.size(); i++) {
-        // Swap current index with i
-        swap(nums[index], nums[i]);
-        // Recur for the next index
-        generatePermutations(nums, index + 1, result);
-        // Backtrack: Swap back to restore the array
-        swap(nums[index], nums[i]);
-    }
-}
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> res;
-        generatePermutations(nums,0,res);
+        vector<int> subset;
+        unordered_set<int> visited;
+        DFS(nums,res,visited,subset);
         return res;
     }
 };
