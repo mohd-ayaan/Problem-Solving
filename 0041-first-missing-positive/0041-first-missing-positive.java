26429ac48ad0b409077e14aedd6fbc712b64dd71
mohd-ayaan <2k22.csaiml.2212406@gmail.com>
2025-09-09 22:23:44 +0530

    Time: 1 ms (100%), Space: 57.3 MB (59.49%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,14 +1,33 @@
 class Solution {
+    // TC:O(n), SC:O(n)
+    // public int firstMissingPositive(int[] nums) {
+    //     HashMap<Integer,Integer> hs=new HashMap<Integer,Integer>();
+    //     for(int i=0;i<nums.length;i++){
+    //         hs.put(nums[i],1);
+    //     }
+    //     for(int i=1;i<nums.length+1;i++){
+    //         if(hs.containsKey(i)==false){
+    //             return i;
+    //         }
+    //     }
+    //     return nums.length + 1;
+    // }
+
+
+    // TC:O(n), SC:O(1)
     public int firstMissingPositive(int[] nums) {
-        HashMap<Integer,Integer> hs=new HashMap<Integer,Integer>();
-        for(int i=0;i<nums.length;i++){
-            hs.put(nums[i],1);
-        }
-        for(int i=1;i<nums.length+1;i++){
-            if(hs.containsKey(i)==false){
-                return i;
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            while(nums[i]>0 && nums[i]<=n && nums[i]!=nums[nums[i] - 1]){
+                int temp=nums[i];
+                nums[i]=nums[temp-1];
+                nums[temp-1]=temp;
             }
         }
-        return nums.length + 1;
+
+        for(int i=0;i<n;i++){
+            if(nums[i]!=i+1){return i+1;}
+        }
+        return n +1;
     }
 }
