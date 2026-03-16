1aef332e50a0a125029fdb40f69b9924afdb4695
mohd-ayaan <143902353+mohd-ayaan@users.noreply.github.com>
2026-03-16 23:45:04 +0530

    Time: 21 ms (72.75%), Space: 45.4 MB (67.37%) - LeetHub

--- 0352-data-stream-as-disjoint-intervals/0352-data-stream-as-disjoint-intervals.java
+++ 0352-data-stream-as-disjoint-intervals/0352-data-stream-as-disjoint-intervals.java
@@ -1,23 +1,55 @@
+//Ineffecient-sorting(nlogn)
+// class SummaryRanges {
+//     List<Integer> arr;
+//     public SummaryRanges() {
+//         arr=new ArrayList<>();
+//     }
+    
+//     public void addNum(int value) {
+//         arr.add(value);
+//         Collections.sort(arr);
+//     }
+    
+//     public int[][] getIntervals() {
+//         List<int[]> res=new ArrayList<>();
+//         int i=0,j,n=arr.size();
+//         while(i<n){
+//             j=i;
+//             while(j<n-1 && (arr.get(j+1)==arr.get(j)+1 || arr.get(j+1)==arr.get(j))){j++;}
+//             res.add(new int[]{arr.get(i),arr.get(j)});
+//             i=j+1;
+//         }
+//         return res.toArray(new int[res.size()][]);
+//     }
+// }
+
+
+//)(Treeset sorting-logn) and get query-O(n)
 class SummaryRanges {
-    List<Integer> arr;
+    private TreeSet<Integer> set;
+
     public SummaryRanges() {
-        arr=new ArrayList<>();
+        set = new TreeSet<>();
     }
-    
+
     public void addNum(int value) {
-        arr.add(value);
-        Collections.sort(arr);
+        set.add(value); // O(log n), no duplicates
     }
-    
+
     public int[][] getIntervals() {
-        List<int[]> res=new ArrayList<>();
-        int i=0,j,n=arr.size();
-        while(i<n){
-            j=i;
-            while(j<n-1 && (arr.get(j+1)==arr.get(j)+1 || arr.get(j+1)==arr.get(j))){j++;}
-            res.add(new int[]{arr.get(i),arr.get(j)});
-            i=j+1;
+        List<int[]> res = new ArrayList<>();
+        Integer start = null, end = null;
+        for (int num : set) {
+            if (start == null) {
+                start = end = num;
+            } else if (num == end + 1) {
+                end = num; // extend interval
+            } else {
+                res.add(new int[]{start, end});
+                start = end = num; // new interval
+            }
         }
+        if (start != null) res.add(new int[]{start, end});
         return res.toArray(new int[res.size()][]);
     }
 }
