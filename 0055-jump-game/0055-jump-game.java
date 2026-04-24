4a472c3e4ae6820ea60a4948402d61a6c9774cdb
mohd-ayaan <143902353+mohd-ayaan@users.noreply.github.com>
2026-04-24 11:44:58 +0530

    Time: 2 ms (86.31%), Space: 47.9 MB (63.68%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,13 +1,13 @@
 class Solution {
     public boolean canJump(int[] nums) {
         int n=nums.length;
-        int maxreach=0, maxind=0;
-        for(int i=0;i<n-1;i++){
-            maxind=(nums[i]!=0)?i+nums[i]:maxind;
-            // System.out.println(i+" "+maxind);
-            maxreach=Math.max(maxreach,maxind);
+        if(n==1){return true;}
+        int farthest=0;
+        for(int i=0;i<n;i++){
+            if(i>farthest){return false;}
+            farthest=Math.max(farthest,i+nums[i]);
         }
-        if(maxreach>=(n-1)){return true;}
-        return false;
+
+        return true;
     }
 }
