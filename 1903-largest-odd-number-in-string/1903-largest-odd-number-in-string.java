206585f7dc15dfae3030d9deef1668a4458437c5
mohd-ayaan <143902353+mohd-ayaan@users.noreply.github.com>
2026-07-10 11:26:28 +0530

    Time: 1 ms (99.8%), Space: 47.1 MB (55.28%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.java
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.java
@@ -1,10 +1,15 @@
 class Solution {
+    // O(N)-time and space(string builder)
     public String largestOddNumber(String num) {
         int len=num.length();
         int ind=-1;
 
-        for(int i=len-1;i>=0;i--){
-            if(num.charAt(i)%2!=0){
+        for(int i=len-1;i>=0;i--){    
+            // if(num.charAt(i)%2!=0)   implicit type casting is done,'0'->48, so logic technically works only by coincidence.
+            // corrent is defined below by 2 ways:
+            // if (Character.getNumericValue(num.charAt(i)) % 2 != 0)
+
+            if((num.charAt(i)-'0')%2!=0){
                 ind=i;
                 break;
             }
